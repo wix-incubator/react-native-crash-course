cd2afd9d5f0a611f04c2bf316c08bb990340cd7a
Dror Biran <drorb@wix.com>
2018-11-01 23:39:02 +0200

    Adding Buttons to the AddPost Screen

--- src/posts/screens/AddPost.js
+++ src/posts/screens/AddPost.js
@@ -1,7 +1,44 @@
 import React, {Component} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import PropTypes from 'prop-types';
+import {Navigation} from 'react-native-navigation/lib/dist/index';
 
 class AddPost extends Component {
+
+  static propTypes = {
+    componentId: PropTypes.string
+  };
+
+  constructor(props) {
+    super(props);
+    Navigation.events().bindComponent(this);
+  }
+
+  static get options() {
+    return {
+      topBar: {
+        title: {
+          text: 'Add Post'
+        },
+        rightButtons: [{
+          id: 'saveBtn',
+          text: 'Save'
+        }],
+        leftButtons: [{
+          id: 'cancelBtn',
+          text: 'Cancel'
+        }]
+      }
+    };
+  }
+
+  navigationButtonPressed({buttonId}) {
+    if (buttonId === 'cancelBtn') {
+      Navigation.dismissModal(this.props.componentId);
+    } else if (buttonId === 'saveBtn') {
+      alert('saveBtn');
+    }
+  }
 
   render() {
     return (

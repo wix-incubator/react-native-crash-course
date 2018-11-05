e71e54325b05adf52cfb8c812d8a914654264e1c
Dror Biran <drorb@wix.com>
2018-11-05 08:51:00 +0200

    Adding on delete button navigation

--- src/posts/screens/ViewPost.js
+++ src/posts/screens/ViewPost.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, Button, StyleSheet} from 'react-native';
+import {Navigation} from 'react-native-navigation';
 import PropTypes from 'prop-types';
 
 
@@ -10,11 +11,29 @@ class ViewPost extends Component {
     somePropToPass: PropTypes.string
   };
 
+  constructor(props) {
+    super(props);
+    this.onPostDeletePressed = this.onPostDeletePressed.bind(this);
+  }
+
+  onPostDeletePressed() {
+    //In here we will request from the server to delete the post
+    Navigation.pop(this.props.componentId);
+    setTimeout(() => {
+      alert('Post deleted');
+    }, 1000);
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.text}>ViewPost Screen</Text>
         <Text>{this.props.somePropToPass}</Text>
+        <Button
+          title="Delete Post"
+          onPress={this.onPostDeletePressed}
+          color={'red'}
+        />
       </View>
     );
   }

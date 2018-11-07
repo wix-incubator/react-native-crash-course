07ff0602abd2f94878542e2b9524212301366b03
Dror Biran <drorb@wix.com>
2018-11-07 16:01:41 +0200

    creating our fake REST api in 30 seconds

--- src/posts/posts.actions.js
+++ src/posts/posts.actions.js
@@ -1,21 +1,8 @@
 import {postsStore} from './posts.store';
 
-const posts = [
-  {
-    id: 1,
-    title: 'Post 1',
-    text: 'Post 1 text',
-    img: 'https://picsum.photos/200/200/?image=977'
-  },
-  {
-    id: 2,
-    title: 'Post 2',
-    text: 'Post 2 text',
-    img: 'https://picsum.photos/200/200/?image=1'
-  }
-];
-
 export function fetchPosts() {
+  // TODO: fetch posts from our fake server
+  const posts = [];
   postsStore.setPosts(posts);
 }
 

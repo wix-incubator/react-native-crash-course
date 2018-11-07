34623ee7e44a9c679be1de24800014a69f1aa8d0
Dror Biran <drorb@wix.com>
2018-11-07 16:08:40 +0200

    fetching our posts from our server

--- src/posts/posts.actions.js
+++ src/posts/posts.actions.js
@@ -1,8 +1,8 @@
 import {postsStore} from './posts.store';
 
-export function fetchPosts() {
-  // TODO: fetch posts from our fake server
-  const posts = [];
+export async function fetchPosts() {
+  const response = await fetch('http://localhost:3000/posts');
+  const posts = await response.json();
   postsStore.setPosts(posts);
 }
 

aa4caf255452d0929ab09f90b6ae82b90a4a086f
Dror Biran <drorb@wix.com>
2019-01-23 11:12:59 +0200

    moving detox to mocha

--- e2e/init.js
+++ e2e/init.js
@@ -1,19 +1,19 @@
 const detox = require('detox');
 const config = require('../package.json').detox;
-const adapter = require('detox/runners/jest/adapter');
+const adapter = require('detox/runners/mocha/adapter');
 
-jest.setTimeout(120000);
-jasmine.getEnv().addReporter(adapter);
-
-beforeAll(async () => {
+before(async () => {
   await detox.init(config);
 });
 
-beforeEach(async () => {
-  await adapter.beforeEach();
+beforeEach(async function () {
+  await adapter.beforeEach(this);
 });
 
-afterAll(async () => {
-  await adapter.afterAll();
+afterEach(async function () {
+  await adapter.afterEach(this);
+});
+
+after(async () => {
   await detox.cleanup();
 });
